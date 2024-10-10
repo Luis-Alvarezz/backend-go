edb57177508e8559bbbe6e5fad7a0ba9b14f2ed7
Luis-Alvarezz <luisangelug10@gmail.com>
2024-10-10 10:17:03 -0600

    Create struct for variables

---
+++ app/models/user.go
@@ -0,0 +1,17 @@
+package models
+
+// Definimos la estructura que vamos a manejar:
+type Usuario struct {
+	ID        string `json:"id"`
+	Nombre    string `json:"nombre"`
+	Apaterno  string `json:"apaterno"`
+	Amaterno  string `json:"amaterno"`
+	Direccion string `json:"direccion"`
+	Telefono  string `json:"telefono"`
+	Ciudad    string `json:"ciudad"`
+	Estado    string `json:"estado"`
+	Usuario   string `json:"usuario"`
+	Password  string `json:"-"`
+	Rol       string `json:"rol"`
+	Imagen    string `json:"imagen"`
+}
