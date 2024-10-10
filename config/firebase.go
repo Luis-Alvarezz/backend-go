a4873032483ebfc9cca1283ba05520b05d249400
Luis-Alvarezz <luisangelug10@gmail.com>
2024-10-10 10:18:55 -0600

    File for Firebase Config and controll Auth with token

---
+++ config/firebase.go
@@ -0,0 +1,30 @@
+package config
+
+// Importacion de Librerias a Utilizar:
+import (
+	"context"
+	"log"
+
+	firebase "firebase.google.com/go"
+	"firebase.google.com/go/auth"
+	"google.golang.org/api/option"
+)
+
+/* funct Hello (name string) string */
+/* funct FunctionName (name Parameter Type) Return Type */
+func InitialFirebaseApp() *firebase.App {
+	opt := option.WithCredentialsFile("./services/firebaseServiceAccount.json") // La ruta de Service Account
+	app, err := firebase.NewApp(context.Background(), nil, opt)                 // Esto es Try-Catch
+	if err != nil {
+		log.Fatalf("Error al inicializar Firabase App: %v", err)
+	}
+	return app
+}
+
+func GetAuthClient(app *firebase.App) *auth.Client {
+	client, err := app.Auth(context.Background())
+	if err != nil {
+		log.Fatalf("Error al obtener el Cliente: %w", err)
+	}
+	return client
+}
