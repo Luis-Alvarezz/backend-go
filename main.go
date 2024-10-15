2dcdd6d7cb369e66a210468dc3f1d607dcda1a4e
Luis-Alvarezz <luisangelug10@gmail.com>
2024-10-14 20:43:10 -0600

    Create file main for work with Firebase

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+// File for initial routes and control of all page
+
+import (
+	"backend-go/app/routes"
+	"backend-go/config"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	// Inicializamos FIrebase:
+	config.InitialFirebaseApp()
+	// Inicializar las rutas:
+	router := mux.NewRouter()
+	routes.InitiallizeRoutes(router)
+	log.Println("Servidor Trabajando en el puerto: 8080 ")
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
